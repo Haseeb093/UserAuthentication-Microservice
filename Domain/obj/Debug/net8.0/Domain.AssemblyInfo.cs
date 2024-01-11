93b046fc87eadb16f8cb28f482fe529dfea84c31
Haseeb093 <haseeb.shahid1@hotmail.com>
2024-01-11 13:06:05 +0500

    Addmng in Ignore FIles

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c73933a0f16b31ee9116dc35070b013e9aa569")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e171598571b324727dc0f425438992e27cf933e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
