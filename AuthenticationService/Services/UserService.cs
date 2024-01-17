d48eeb58611a8a6c23d9a4b5273d024ec7de6a0e
Haseeb093 <haseeb.shahid1@hotmail.com>
2024-01-17 18:53:02 +0500

    17 jan 2024

--- AuthenticationService/Services/UserService.cs
+++ AuthenticationService/Services/UserService.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Service.Services;
 using Service.Validation;
 using Services.ApplicationContext;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -125,6 +126,7 @@ namespace AuthenticationService
         public async Task<ResponseObject<List<Error>>> LockOutUser(LockOutUserDto lockOutUser)
         {
             var response = new ResponseObject<List<Error>>();
+
             if (await LockOutValidation(lockOutUser, response))
             {
                 if (IdentityResponseValidation(await _userManager.SetLockoutEndDateAsync(user, DateTime.Now.AddYears(10)), response))
@@ -139,6 +141,7 @@ namespace AuthenticationService
         public async Task<ResponseObject<List<Error>>> UnlockUser(LockOutUserDto lockOutUser)
         {
             var response = new ResponseObject<List<Error>>();
+
             if (await LockOutValidation(lockOutUser, response))
             {
                 if (IdentityResponseValidation(await _userManager.SetLockoutEndDateAsync(user, DateTime.Now.AddMinutes(1)), response))
@@ -153,17 +156,40 @@ namespace AuthenticationService
         public async Task<ResponseObject<List<UserDto>>> GetAllDoctors()
         {
             var response = new ResponseObject<List<UserDto>>();
-            //var ss1 = await _userManager.Users.ToListAsync();
-            //var x = from U in Users
-            //        join R in Roles on equals userRole.UserId
-            //        join role in t.Roles on userRole.RoleId equals role.Id into roles
-            //        select new
-            //        {
-            //            User = usr,
-            //            Roles = roles
-            //        };
-
-            response.Data = _mapper.Map<List<UserDto>>(await _userManager.Users.ToListAsync());
+            response.Data = await (from user in _userManager.Users
+                                   join userRoles in _dbContext.UserRoles on user.Id equals userRoles.UserId
+                                   join roles in _dbContext.Roles on userRoles.RoleId equals roles.Id
+                                   join gender in _dbContext.Genders on (user as Users).GenderId equals gender.GenderId
+                                   join department in _dbContext.Departments on (user as Users).DepartmentId equals department.DepartmentId
+                                   join country in _dbContext.Countries on (user as Users).CountryId equals country.CountryId
+                                   join state in _dbContext.States on (user as Users).StateId equals state.StateId
+                                   join city in _dbContext.Cities on (user as Users).CityId equals city.CityId
+                                   where roles.Name == Roles.Doctor.ToString()
+                                   select new UserDto
+                                   {
+                                       Id = Helper.EncryptString(user.Id),
+                                       FirstName = (user as Users).FirstName,
+                                       LastName = (user as Users).LastName,
+                                       UserName = user.UserName,
+                                       Email = user.Email,
+                                       Role = roles.Name,
+                                       GenderId = gender.GenderId,
+                                       Gender = gender.Name,
+                                       DateofBirth = (user as Users).DateofBirth.ToString(),
+                                       Address1 = (user as Users).Address1,
+                                       Address2 = (user as Users).Address2,
+                                       PostalCode = (user as Users).PostalCode,
+                                       PhoneNumber = (user as Users).PhoneNumber,
+                                       SecondaryPhoneNumber = (user as Users).SecondaryPhoneNumber,
+                                       DepartmentId = department.DepartmentId,
+                                       Department = department.Name,
+                                       CountryId = country.CountryId,
+                                       Country = country.Name,
+                                       StateId = state.StateId,
+                                       State = state.Name,
+                                       CityId = city.CityId,
+                                       City = city.Name
+                                   }).ToListAsync();
             Helper.SetSuccessRespose(response);
             return response;
         }
@@ -171,7 +197,40 @@ namespace AuthenticationService
         public async Task<ResponseObject<List<UserDto>>> GetAllPatients()
         {
             var response = new ResponseObject<List<UserDto>>();
-            response.Data = _mapper.Map<List<UserDto>>(await _dbContext.Users.ToListAsync());
+            response.Data = await (from user in _userManager.Users
+                                   join userRoles in _dbContext.UserRoles on user.Id equals userRoles.UserId
+                                   join roles in _dbContext.Roles on userRoles.RoleId equals roles.Id
+                                   join gender in _dbContext.Genders on (user as Users).GenderId equals gender.GenderId
+                                   join department in _dbContext.Departments on (user as Users).DepartmentId equals department.DepartmentId
+                                   join country in _dbContext.Countries on (user as Users).CountryId equals country.CountryId
+                                   join state in _dbContext.States on (user as Users).StateId equals state.StateId
+                                   join city in _dbContext.Cities on (user as Users).CityId equals city.CityId
+                                   where roles.Name == Roles.Patient.ToString()
+                                   select new UserDto
+                                   {
+                                       Id = Helper.EncryptString(user.Id),
+                                       FirstName = (user as Users).FirstName,
+                                       LastName = (user as Users).LastName,
+                                       UserName = user.UserName,
+                                       Email = user.Email,
+                                       Role = roles.Name,
+                                       GenderId = gender.GenderId,
+                                       Gender = gender.Name,
+                                       DateofBirth = (user as Users).DateofBirth.ToString(),
+                                       Address1 = (user as Users).Address1,
+                                       Address2 = (user as Users).Address2,
+                                       PostalCode = (user as Users).PostalCode,
+                                       PhoneNumber = (user as Users).PhoneNumber,
+                                       SecondaryPhoneNumber = (user as Users).SecondaryPhoneNumber,
+                                       DepartmentId = department.DepartmentId,
+                                       Department = department.Name,
+                                       CountryId = country.CountryId,
+                                       Country = country.Name,
+                                       StateId = state.StateId,
+                                       State = state.Name,
+                                       CityId = city.CityId,
+                                       City = city.Name
+                                   }).ToListAsync();
             Helper.SetSuccessRespose(response);
             return response;
         }
